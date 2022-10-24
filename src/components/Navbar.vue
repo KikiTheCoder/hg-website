961c07c50c7ae07d5c8c899b38786be32b4d40bb
KikiTheCoder <kikilp07@web.de>
2022-10-24 22:39:39 +0200

    inital commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav id="topnav" class="border-b border-neutral-700 w-screen fixed top-0 left-0 h-20 flex px-8 text-white bg-neutral-900">
+        <div class="text-xl my-auto font-halloween">Halloween Garten Hohenkirchen</div>
+        <div class="ml-auto flex">
+            <a class="nav-btn">Home</a>
+            <a class="nav-btn">About</a>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+@tailwind components;
+@layer components {
+    .nav-btn {
+        @apply text-xl mr-3 my-auto font-halloween float-right hover:text-red-700 cursor-pointer duration-200;
+    }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-neutral-900">
+    <div class="h-screen text-white flex">
+      <div class="font-halloween m-auto text-6xl text-center">
+        Let the
+        <div class="text-9xl text-red-700 my-8">Horror</div>
+        begin
+      </div>
+
+      <v-icon class="text-white absolute bottom-20 left-[calc(99vw/2)]" scale=2 animation="pulse" name="md-keyboarddoublearrowdown"></v-icon>
+    </div>
+
+    <div class="h-screen border-t border-neutral-700">
+      <div class="mt-20 text-white">
+        blob
+      </div>
+    </div>
+  </div>
+</template>
