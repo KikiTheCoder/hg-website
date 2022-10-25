5cfa1a77d5331ab3e1d939bd9c763db6541d7cf6
KikiTheCoder <kikilp07@web.de>
2022-10-25 13:31:13 +0200

    2nd commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,10 @@
 <template>
-    <nav id="topnav" class="border-b border-neutral-700 w-screen fixed top-0 left-0 h-20 flex px-8 text-white bg-neutral-900">
-        <div class="text-xl my-auto font-halloween">Halloween Garten Hohenkirchen</div>
+    <nav id="topnav" class="z-50 border-b border-neutral-700 w-screen fixed top-0 left-0 h-20 flex px-8 text-white bg-neutral-900">
+        <div class="text-xl my-auto font-halloween">
+            <a href="/" class="hover:text-red-700 duration-200">
+                Halloween Garten Hohenkirchen
+            </a>
+        </div>
         <div class="ml-auto flex">
             <a class="nav-btn">Home</a>
             <a class="nav-btn">About</a>
@@ -12,7 +16,7 @@
 @tailwind components;
 @layer components {
     .nav-btn {
-        @apply text-xl mr-3 my-auto font-halloween float-right hover:text-red-700 cursor-pointer duration-200;
+        @apply text-xl mr-6 my-auto font-halloween float-right hover:text-red-700 cursor-pointer duration-200;
     }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
+document.title += " - Home"
 </script>
 
 <template>
   <div class="bg-neutral-900">
-    <div class="h-screen text-white flex">
+    <div class="h-[85vh] pt-40 text-white flex">
       <div class="font-halloween m-auto text-6xl text-center">
         Let the
         <div class="text-9xl text-red-700 my-8">Horror</div>
         begin
       </div>
 
-      <v-icon class="text-white absolute bottom-20 left-[calc(99vw/2)]" scale=2 animation="pulse" name="md-keyboarddoublearrowdown"></v-icon>
+    </div>
+    <div class="h-[15vh] flex">
+      <a href="#blob" class="m-auto mb-20">
+        <v-icon scale=2 inverse="true" animation="pulse" name="md-keyboarddoublearrowdown"></v-icon>
+      </a>
     </div>
 
     <div class="h-screen border-t border-neutral-700">
-      <div class="mt-20 text-white">
+      <div class="mt-20 text-white" id="blob">
         blob
       </div>
     </div>
